b11ed56f3f2a6669b11d477ce8565cfe14bd9bdf
Pandiful <stofkerna@gmail.com>
2023-01-16 11:42:37 -0300

    initial setup of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
